6b84b928621120de3192d52d5abd1ac82719016b
o1pwe <o1pwerbot@yandex.ru>
2023-08-24 01:06:54 +0300

    Add more endpoints

--- movies_api/urls.py
+++ movies_api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from movies_api.views import MovieListView, ReviewAddView
+from movies_api.views import MovieListAPIView, ReviewAddView, MovieSearchAPIView, MovieDetailAPIView
 
 app_name = 'movies_api'
 
 urlpatterns = [
-    path('movies/', MovieListView.as_view(), name='movie_list'),
+    path('movies/', MovieListAPIView.as_view(), name='movie_list'),
+    path('movies/search/', MovieSearchAPIView.as_view(), name='movie_search'),
+    path('movies/<slug:genre>/<int:film_id>/', MovieDetailAPIView.as_view(), name='movie_detail'),
     path('reviews/', ReviewAddView.as_view(), name='add_review')
 ]
--- movies_api/views.py
+++ movies_api/views.py
@@ -1,17 +1,28 @@
 from django.db.models import Q
 from django.http import HttpResponse
 # Create your views here.
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
-from movies_api.models import Movie, Review
+from movies_api.models import Movie, Review, Genre
 from movies_api.serializers.movie_serializers import MovieSerializer
 from movies_api.serializers.review_serializers import ReviewSerializer
 
 
-class MovieListView(generics.ListAPIView):
+class CustomPagination(PageNumberPagination):
+    page_size = 25  # Number of items per page
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class MovieListAPIView(generics.ListAPIView):
     model = Movie
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    pagination_class = CustomPagination
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -19,15 +30,20 @@ class MovieListView(generics.ListAPIView):
         filter_params = self.request.GET.dict()
 
         # Apply filters dynamically
-        queryset, order_by_param = self.apply_filters(queryset, filter_params)
+        queryset, order_by_param, page_size = self.apply_filters(queryset, filter_params)
 
         # Apply order-by dynamically
         if order_by_param:
             queryset = self.apply_ordering(queryset, order_by_param)
 
-        return queryset
+        # Set the page size for pagination
+        if page_size:
+            self.pagination_class.page_size = page_size
+
+        return self.paginate_queryset(queryset)  # Use paginate_queryset method
 
     def apply_filters(self, queryset, filter_params):
+        page_size = filter_params.pop('page_size', None)
         q_objects = Q()
         # Handle required directors
         if directors_required := filter_params.pop('directors_required', None):
@@ -60,7 +76,7 @@ class MovieListView(generics.ListAPIView):
         q_objects &= Q(**filter_params)
         queryset = queryset.filter(q_objects)
 
-        return queryset, order_by_param
+        return queryset, order_by_param, page_size
 
     def apply_ordering(self, queryset, order_by_params):
         # Validate and sanitize order-by fields
@@ -68,6 +84,38 @@ class MovieListView(generics.ListAPIView):
         sanitized_order_by = [field for field in order_by_params if field.lstrip('-') in valid_fields]
 
         return queryset.order_by(*sanitized_order_by)
+
+
+class MovieSearchAPIView(ListAPIView):
+    serializer_class = MovieSerializer
+    queryset = Movie.objects.all()
+
+    def get_queryset(self):
+        if query := self.request.query_params.get('q'):
+            return Movie.objects.filter(Q(title__icontains=query))
+        return Movie.objects.none()
+
+
+class MovieDetailAPIView(generics.RetrieveAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        genre_name = self.kwargs.get('genre')
+        film_id = self.kwargs.get('film_id')
+
+        try:
+            genre = Genre.objects.get(Q(name__iexact=genre_name))
+            if movie := Movie.objects.filter(main_genre=genre, id=film_id).first():
+                serializer = self.get_serializer(movie)
+                return Response(serializer.data)
+
+            return Response({'detail': 'Movie not found'}, status=404)
+        except Genre.DoesNotExist:
+            return Response({'detail': 'Genre not found'}, status=404)
+
+
+
 
 
 class ReviewAddView(generics.CreateAPIView):
@@ -79,4 +127,4 @@ class ReviewAddView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return HttpResponse(status=201)
+        return Response(status=201)
